79b81db1876008098f3445b097629d787c6b0b7e
Shov1bow1 <fireblaster2001@gmail.com>
2022-05-14 13:39:42 +0700

    hoang commit

--- routes/customer.js
+++ routes/customer.js
@@ -1,15 +1,17 @@
 
 var express = require('express');
-const router  = express.Router();
-const session=require('express-session');
-const tempRun=express();
-const con=require('../mysql');
-const stripe=require('../stripe');
+const router = express.Router();
+const session = require('express-session');
+const tempRun = express();
+const con = require('../mysql');
+const stripe = require('../stripe');
+const crypto = require("crypto");
+let algorithm = "sha256";
 tempRun.use(session({
-    resave:true,
-    saveUninitialized:true,
-    secret:'somesecret',
-    cookie:{maxAge:1000*60*60*24}
+    resave: true,
+    saveUninitialized: true,
+    secret: 'somesecret',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }));
 router.post("/create-payment-intent", async (req, res) => {
     const { items } = req.body;
@@ -35,111 +37,206 @@ const calculateOrderAmount = (items) => {
     return 1400;
 };
 ////
-//// CUSTOMER LOGIN
+//// CUSTOMER 
 ////
-router.post("/Register",(req,res)=>{
+//Register
+router.post("/Register", (req, res) => {
     var numericId;
-    const getNumericId=(data)=>{
-        numericId=data;     
-        console.log(numericId); 
-        numericId+=1;
-        const CUSTOMER_ID='CUS'+numericId;
+
+    const getNumericId = (data) => {
+        numericId = data;
+        console.log(numericId);
+        numericId += 1;
+        const CUSTOMER_ID = 'CUS' + numericId;
         console.log(CUSTOMER_ID);
-       
-        if(req.body.CUSTOEMR_NAME===undefined||
-            req.body.CUSTOMER_NAME===null||
-            req.body.CUSTOMER_NAME.length===0||
-            req.body.CUSTOMER_ID===undefined||
-            req.body.CUSTOMER_ID===null){
+        console.log(req.body.CUSTOMER_NAME);
+        if (req.body.CUSTOMER_NAME === undefined ||
+            req.body.CUSTOMER_NAME === null ||
+            req.body.CUSTOMER_NAME.length === 0 ||
+            CUSTOMER_ID === undefined ||
+            CUSTOMER_ID === null) {
             console.log("ending here");
             res.end();
         }
-        else{
+        else {
             con.query("insert into CUSTOMER_INFO(CUSTOMER_ID,FULL_NAME,GENDER,DATE_OF_BIRTH,POINT_AVAILABLE) values ('" + CUSTOMER_ID + "','" +
-                req.body.CUSTOEMR_NAME + "','" +
-                req.body.GENDER + "','"+
-                req.body.BIRTHDAY+"',0;);"
+                req.body.CUSTOMER_NAME + "','" +
+                req.body.GENDER + "','" +
+                req.body.BIRTHDAY + "',0);"
                 , (err, result) => {
                     if (err) throw (err);
-        })}
-        if(req.body.CUSTOMER_EMAIL===null||
-            req.body.CUSTOMER_EMAIL===undefined||
-            req.body.CUSTOMER_EMAIL.length===0||
-            req.body.CUS_PASSWORD===undefined||
-            req.body.CUS_PASSWORD===null||
-            req.body.CUS_PASSWORD.length===0)
-            {
-                res.end();
-            }
-        else{
+                })
+        }
+        if (req.body.CUSTOMER_EMAIL === null ||
+            req.body.CUSTOMER_EMAIL === undefined ||
+            req.body.CUSTOMER_EMAIL.length === 0 ||
+            req.body.CUS_PASSWORD === undefined ||
+            req.body.CUS_PASSWORD === null ||
+            req.body.CUS_PASSWORD.length === 0) {
+            res.end();
+        }
+        else {
             con.query("insert into CUSTOMER_SECURITY(CUSTOMER_ID,CUSTOMER_EMAIL,CUS_PASSWORD) values ('" + CUSTOMER_ID +
                 "','" + req.body.CUSTOMER_EMAIL +
                 "','" + req.body.CUS_PASSWORD + "');"
                 , (err, result) => {
                     if (err) throw (err);
-                    
+
                 });
         }
-    }  
-    con.query("select CUSTOMER_EMAIL from CUSTOMER_SECURITY where CUSTOMER_EMAIL='"+req.body.CUSTOMER_EMAIL+"';",(err,result)=>{
-        if(err) throw err;
-        if(result[0]!==undefined||result[0].CUSTOMER_EMAIL.length!==0||result[0].CUSTOMER_EMAIL!==null){
-            res.send([{"CUSTOMER_EMAIL":"Already exist Email;"}])
-            res.end();
-        } 
-        else{
-            con.query("select COUNT(*) as NUMBER from CUSTOMER_SECURITY", (err, result) => {
-                if (err) throw err;
-                console.log(result[0].NUMBER);
-                return getNumericId(result[0].NUMBER);
-            })
-        }       
-    })
-})
-router.post("/Login",(req,res)=>{
-    con.query(
-    "select * from CUSTOMER_SECURITY,CUSTOMER_INFO where CUSTOMER_EMAIL='"+req.body.CUSTOMER_EMAIL
-    +"' and CUS_PASSWORD = '"+req.body.PASSWORD
-    +"' and CUSTOMER_SECURITY.CUSTOMER_ID=CUSTOMER_INFO.CUSTOMER_ID;",(err,result)=>{
-        if(err) throw err;
-        if(result.length===0) {res.send("Please enter your Account");}
-        else{
-            console.log(result);
-            session.CUSTOMER_ID = result[0].CUSTOMER_ID;
-            session.CUSTOMER_NAME = result[0].FULL_NAME;
-            const BackData=
-            [
-                {
-                    CUSTOMER_ID:session.CUSTOMER_ID,
-                    CUSTOMER_NAME:session.CUSTOMER_NAME
+    }
+    if (session.CUSTOMER_ID !== undefined && session.CUSTOMER_NAME !== undefined
+        && session.CUSTOMER_ID !== null && session.CUSTOMER_NAME !== null && session.TOKEN !== null) {
+        con.query("select * from CUSTOMER_SECURITY where CUSTOMER_ID='" + session.CUSTOMER_ID + "';", (err, result) => {
+            if (err) throw err;
+            if (
+                result[0] !== null
+            ) {
+                let hashLogin = crypto.createHash(algorithm).update(result[0].CUS_PASSWORD + result[0].CUSTOMER_EMAIL).digest("hex");
+                if (hashLogin === session.TOKEN) {
+                    res1.send([
+                        {
+                            CUSTOMER_ID: session.CUSTOMER_ID,
+                            CUSTOMER_NAME: session.CUSTOMER_NAME
+                        }
+                    ]);
                 }
-            ];
-            res.send(JSON.stringify(BackData));
-        } 
-    })
-})
-router.get("/LoginSession",(req1,res1)=>{
-    console.log(session.CUSTOMER_NAME);
-    if((session.CUSTOMER_ID===undefined && session.CUSTOMER_NAME===undefined) 
-        ||(session.CUSTOMER_ID===null && session.CUSTOMER_NAME===undefined) 
-        ||(session.CUSTOMER_ID===undefined && session.CUSTOMER_NAME===null)
-        ||(session.CUSTOMER_ID===null && session.CUSTOMER_NAME===null))
-    {
-        res1.send([
-            {
-                CUSTOMER_ID:session.CUSTOMER_ID,
-                CUSTOMER_NAME:session.CUSTOMER_NAME
             }
-        ]);
+        })
+
+    }
+    else {
+        con.query("select CUSTOMER_EMAIL from CUSTOMER_SECURITY where CUSTOMER_EMAIL='" + req.body.CUSTOMER_EMAIL + "';", (err, result) => {
+            console.log(result);
+            if (err) {
+                res.send(err)
+            }
+            else {
+                if (result[0] !== undefined) {
+                    res.send([{ "CUSTOMER_EMAIL": "Already exist Email;" }])
+                }
+                else {
+                    con.query("select COUNT(*) as NUMBER from CUSTOMER_SECURITY", (err, result) => {
+                        if (err) throw err;
+                        console.log(result[0].NUMBER);
+                        return getNumericId(result[0].NUMBER);
+                    })
+                }
+            }
+        })
+    }
+
+})
+//LOGIN USING EMAIL
+router.post("/LoginEmail", (req, res) => {
+    console.log(req.body.CUSTOMER_EMAIL);
+    con.query(
+        "select * from CUSTOMER_SECURITY,CUSTOMER_INFO where CUSTOMER_EMAIL='" + req.body.CUSTOMER_EMAIL
+        + "' and CUS_PASSWORD = '" + req.body.CUS_PASSWORD
+        + "' and CUSTOMER_SECURITY.CUSTOMER_ID=CUSTOMER_INFO.CUSTOMER_ID;", (err, result) => {
+            if (err) throw err;
+            if (result.length === 0) { res.send("Please reenter your Account"); }
+            else {
+                console.log(result);
+                session.TOKEN = crypto.createHash(algorithm).update(req.body.CUS_PASSWORD + req.body.CUSTOMER_EMAIL).digest("hex");
+                session.CUSTOMER_ID = result[0].CUSTOMER_ID;
+                session.CUSTOMER_NAME = result[0].FULL_NAME;
+                const BackData =
+                    [
+                        {
+                            CUSTOMER_ID: session.CUSTOMER_ID,
+                            CUSTOMER_NAME: session.CUSTOMER_NAME,
+                            CUSTOMER_TOKEN: session.TOKEN,
+                        }
+                    ];
+                res.send(JSON.stringify(BackData));
+            }
+        })
+})
+//LOGIN USING Phone Number
+router.post("/LoginPhoneNumber", (req, res) => {
+    console.log(req.body.CUSTOMER_EMAIL);
+    if (session.CUSTOMER_ID !== undefined && session.CUSTOMER_NAME !== undefined
+        && session.CUSTOMER_ID !== null && session.CUSTOMER_NAME !== null && session.TOKEN !== null) {
+        con.query("select * from CUSTOMER_SECURITY where CUSTOMER_ID='" + session.CUSTOMER_ID + "';", (err, result) => {
+            console.log("check");
+            if (err) throw err;
+            if (
+                result[0] !== null
+            ) {
+                let hashLogin = crypto.createHash(algorithm).update(result[0].CUS_PASSWORD + result[0].CUSTOMER_EMAIL).digest("hex");
+                if (hashLogin === session.TOKEN) {
+                    res.send([
+                        {
+                            CUSTOMER_ID: session.CUSTOMER_ID,
+                            CUSTOMER_NAME: session.CUSTOMER_NAME
+                        }
+                    ]);
+                }
+            }
+        })
+    }
+    else {
+        con.query(
+            "select * from PROVIDER__USER_INFO,CUSTOMER_INFO where CUSTOMER_EMAIL='" + req.body.CUSTOMER_PHONE
+            + "' and CUS_PASSWORD = '" + req.body.CUS_PASSWORD
+            + "' and CUSTOMER_SECURITY.CUSTOMER_ID=CUSTOMER_INFO.CUSTOMER_ID;", (err, result) => {
+                if (err) throw err;
+                if (result.length === 0) { res.send("Please reenter your Account"); }
+                else {
+                    console.log(result);
+                    session.TOKEN = crypto.createHash(algorithm).update(req.body.CUS_PASSWORD + req.body.CUSTOMER_EMAIL).digest("hex");
+                    session.CUSTOMER_ID = result[0].CUSTOMER_ID;
+                    session.CUSTOMER_NAME = result[0].FULL_NAME;
+                    const BackData =
+                        [
+                            {
+                                CUSTOMER_ID: session.CUSTOMER_ID,
+                                CUSTOMER_NAME: session.CUSTOMER_NAME,
+                                CUSTOMER_TOKEN: session.TOKEN,
+                            }
+                        ];
+                    res.send(JSON.stringify(BackData));
+                }
+            })
+    }
+})
+//Check Session
+router.get("/Session",(req,res)=>{
+    if (session.CUSTOMER_ID !== undefined && session.CUSTOMER_NAME !== undefined
+        && session.CUSTOMER_ID !== null && session.CUSTOMER_NAME !== null && session.TOKEN !== null) {
+        con.query("select * from CUSTOMER_SECURITY where CUSTOMER_ID='" + session.CUSTOMER_ID + "';", (err, result) => {
+            console.log("check");
+            if (err) throw err;
+            if (result[0] !== null) {
+                let hashLogin = crypto.createHash(algorithm).update(result[0].CUS_PASSWORD + result[0].CUSTOMER_EMAIL).digest("hex");
+                if (hashLogin === session.TOKEN) {
+                    res.send([
+                        {
+                            CUSTOMER_ID: session.CUSTOMER_ID,
+                            CUSTOMER_NAME: session.CUSTOMER_NAME,
+                            TOKEN:session.TOKEN
+                        }
+                    ]);
+                }
+            }
+        })
     }
     else{
-        res1.send([{"message":"Your are now log in"}]);
+        res.send([
+            {
+                CUSTOMER_ID: "null",
+                CUSTOMER_NAME: "null",
+                TOKEN: "null"
+            }
+        ])
     }
-    res1.end();
 })
-router.get("/Logout",(req,res)=>{
-    session.CUSTOMER_ID=null;
-    session.CUSTOMER_NAME=null;
+//LOGOUT
+router.get("/Logout", (req, res) => {
+    session.TOKEN = null;
+    session.CUSTOMER_ID = null;
+    session.CUSTOMER_NAME = null;
     res.end();
 })
-module.exports=router;
+module.exports = router;
--- routes/partner.js
+++ routes/partner.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const session = require("express-session");
+const crypto=require("crypto");
 const conn = require("../mysql.js");
 const tempRun = express();
 const route = express.Router();
+const algorithm="sha256"
 tempRun.use(session({
     resave: true,
     saveUninitialized: true,
@@ -10,6 +12,7 @@ tempRun.use(session({
     cookie: { maxAge: 1000 * 60 * 60 }
 }))
 
+// Register a Partner Account
 route.post("/Register", ((req, res) => {
     var numericId;
     const getNumericId = (data) => {
@@ -24,7 +27,7 @@ route.post("/Register", ((req, res) => {
             req.body.APP_ID === undefined ||
             req.body.APP_ID === null) {
             console.log("ending here");
-            res.end();
+            res.end(); 
         }
         else {
             conn.query("insert into PARTNER_INFO(PARTNER_ID,APP_ID,PARTNER_NAME) values ('" + PARTNER_ID + "','" +
@@ -48,6 +51,7 @@ route.post("/Register", ((req, res) => {
                 "','" + req.body.PARTNER_PASSWORD + "');"
                 , (err, result) => {
                     if (err) throw (err);
+                    session.TOKEN=crypto.createHash(algorithm).update(req.body.PARTNER_EMAIL+req.body.PARTNER_PASSWORD).digest("hex");
                     session.PARTNER_ID = PARTNER_ID;
                     session.PARTNER_NAME = req.body.PARTNER_NAME;
                     res.send([{ "MESSAGE": "Register Success" }]);
@@ -58,14 +62,13 @@ route.post("/Register", ((req, res) => {
     if (req.body.PARTNER_EMAIL === null ||
         req.body.PARTNER_EMAIL === undefined ||
         req.body.PARTNER_EMAIL.length === 0) {
-        res.end();
+        res.send();
     }
     else {
         conn.query("select PARTNER_EMAIL from PARTNER_SECURITY where PARTNER_EMAIL='" + req.body.PARTNER_EMAIL + "';", (err, result) => {
             if (err) { res.end() }
             if (result[0] !== undefined) {
                 res.send([{ "PARTNER_EMAIL": "Already exist Email;" }])
-                res.end();
             }
             else {
                 conn.query("select COUNT(*) as NUMBER from PARTNER_SECURITY", (err, result) => {
@@ -77,12 +80,18 @@ route.post("/Register", ((req, res) => {
         });
     }
 }))
+
+// Partner Login
+
 route.post("/Login", ((req, res) => {
+    console.log(req.body.PARTNER_EMAIL.length);
+    if(req.body.PARTNER_EMAIL!==null && req.body.PARTNER_EMAIL!==undefined||req.body.PARTNER_PASSWORD!==null&&req.body.PARTNER_PASSWORD!==undefined)
     conn.query("select * from PARTNER_SECURITY,PARTNER_INFO where PARTNER_EMAIL='" + req.body.PARTNER_EMAIL +
         "' and PARTNER_PASSWORD='" + req.body.PARTNER_PASSWORD +
-        "' and PARTNER_SECURITY.PARTNER_ID = PARTNER_INFO.PARTNER_ID;", (err, result) => {
+        "' and PARTNER_SECURITY.PARTNER_ID = PARTNER_INFO.PARTNER_ID and PARTNER_INFO.APP_ID='"+req.body.APP_ID+"';", (err, result) => {
+            console.log(result);
             if (err) throw err;
-            if (result[0].PARTNER_ID.length === 0) res.end();
+            if (result.length===0) res.end();
             else {
                 session.PARTNER_ID = result[0].PARTNER_ID;
                 session.PARTNER_NAME = result[0].PARTNER_NAME;
@@ -92,11 +101,27 @@ route.post("/Login", ((req, res) => {
                 }]));
             }
         })
+    else(
+        res.send(JSON.stringify([
+            {
+                message: "Please Enter Your Password and Your Email"
+            }
+        ]))
+    )
 }));
-route.get("/LoginSession", (req, res) => {
+
+// Check Session
+
+route.get("/Session", (req, res) => {
     console.log(session.PARTNER_NAME);
-    if (session.PARTNER_ID === undefined || session.PARTNER_NAME === null || session.PARTNER_ID === null || session.PARTNER_NAME === undefined) {
-        res.send([{ "message": "Your need to log in" }]);
+    if (session.PARTNER_ID === undefined || session.PARTNER_NAME === null ||
+        session.PARTNER_ID === null || session.PARTNER_NAME === undefined ||
+        session.TOKEN === undefined || session.TOKEN === null) {
+        res.send([{ 
+            TOKEN: "null",
+            PARTNER_ID:"null",
+            PARTNER_NAME:"null"
+     }]);
     }
     else {
         res.send([
@@ -108,7 +133,11 @@ route.get("/LoginSession", (req, res) => {
     }
     res.end();
 });
+
+// Partner log out
+
 route.get("/Logout", (req, res) => {
+    session.TOKEN=null;
     session.PARTNER_ID = null;
     session.PARTNER_NAME = null;
     res.end();
