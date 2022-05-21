f49391cd6ca84904b6574a518604d23422777733
Shov1bow1 <fireblaster2001@gmail.com>
2022-05-21 13:47:47 +0700

    hoang commit

--- routes/customer.js
+++ routes/customer.js
@@ -8,13 +8,17 @@ const stripe = require('../stripe');
 const crypto = require("crypto");
 const e = require('express');
 const { stringify } = require('querystring');
-var http = require('http');
+const jwt=require('jsonwebtoken');
 const axios = require('axios');
 const { profile } = require('console');
 const { response } = require('express');
 var ARRAY_APP_ID = ["PROFILE", "FLIGHT", "HOTEL", "AIRPORT", "APART", "XPERIENCE", "CARRENTAL", "EATS", "VOUCHER", "COMBO"];
-let algorithm = "sha256";
-
+let privateKey = "CoTu";
+const TOKEN=jwt.sign({foo:'bar'},privateKey,{algorithm:'RS256'});
+const test={foo:'bar'}
+console.log(TOKEN);
+const SecondToken=jwt.sign(test,privateKey,{algorithm:'RS256'});
+console.log(SecondToken);
 router.post("/create-payment-intent", async (req, res) => {
     const items = 3000;
     // Create a PaymentIntent with the order amount and currency
@@ -112,14 +116,15 @@ router.post("/LoginEmail", (req, res) => {
             res.send([{ ERROR: "please type in Your Password" }]);
         }
         else {
+            console.log(req.body.CUSTOMER_EMAIL);
             con.query(
                 "select * from CUSTOMER_SECURITY,CUSTOMER_INFO where CUSTOMER_EMAIL='" + req.body.CUSTOMER_EMAIL.toUpperCase()
                 + "' and CUS_PASSWORD = '" + req.body.CUS_PASSWORD
                 + "' and CUSTOMER_SECURITY.CUSTOMER_ID=CUSTOMER_INFO.CUSTOMER_ID;", (err, result) => {
-                    if (err) throw err;
-                    if (!result) { res.send("Please reenter your Account"); }
+                    if (err) res.end();
+                    if (!result[0]) { res.send("Please reenter your Account"); }
                     else {
-                   
+                        console.log(result[0]);
                         const TOKEN = crypto.createHash(algorithm).update(req.body.CUS_PASSWORD + req.body.CUSTOMER_EMAIL.toUpperCase()).digest("hex");     
                        
                         const BackData =
@@ -158,7 +163,7 @@ router.post("/getAppId", (req, res) => {
         res.send([{ APP_ID: req.body.APP_ID }]);
     }
     else {
-        res.send([{ APP_ID: "PROFILE" }]);
+        res.send([{ APP_ID: "FLIGHT" }]);
     }
 })
 // send Account info
@@ -226,171 +231,17 @@ router.post(`/getUserInfo`, (req, res) => {
     }
 })
 /// Function SendBackUserInfo
-function handleServerUserInfo(CUSTOMER_INFO_PACKAGE, req, res) {
+function handleServerUserInfo(CUSTOMER_INFO_PACKAGE, req, res, callback) {
     console.log("confirm");
-    switch (req.body.APP_ID) {
-        case "PROFILE":
-            return (res.send({
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }));
-        case "FLIGHT": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-        case "HOTEL": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-        case "AIRPORT": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-
-        }
-        case "APART": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-        case "XPERIENCE": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-        case "CARRENTAL": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                });
-            res.end();
-            break;
-        }
-        case "EATS": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-        case "VOUCHER": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-        case "COMBO": {
-            axios.post('http://localhost:8021/demo', {
-                CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
-                STATUS: req.body.STATUS,
-                CUSTOMER_ID: req.body.CUSTOMER_ID,
-                CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
-                EXPIRED_TIME: req.body.EXPIRED_TIME,
-            }).then(res => {
-                console.log(`statusCode: ${res.status}`);
-                console.log(res);
-            })
-                .catch(error => {
-                    console.error(error);
-                })
-            res.end();
-            break;
-        }
-    }
+    const CUSTOMER_PACKAGE_ALL=[{
+        CUSTOMER_PACKAGE: CUSTOMER_INFO_PACKAGE,
+        STATUS: req.body.STATUS,
+        CUSTOMER_ID: req.body.CUSTOMER_ID,
+        CUSTOMER_TOKEN: req.body.CUSTOMER_TOKEN,
+        EXPIRED_TIME: req.body.EXPIRED_TIME,
+    }]
+    res.send([{
+        PACKAGE: CUSTOMER_PACKAGE_ALL
+    }]);
 }
 module.exports = router;
